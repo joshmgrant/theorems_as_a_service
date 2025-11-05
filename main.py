022458ef7d78453be0dde141e781c788e5f32e17
Josh Grant <joshua.m.grant@gmail.com>
2025-11-05 11:09:08 -0500

    some html added

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
-@app.get('/index.html')
+@app.get("/", response_class=HTMLResponse)
+@app.get('/index.html', response_class=HTMLResponse)
 async def root():
-    return {"message": "Welcome to Theorems as a Service"}
-
+    return """
+    <html>
+        <head>
+            <title>Theorems as a Service</title>
+        </head>
+        <body>
+            <h1>Welcome to Theorems as a Service!</h1>
+            <div>Here's a math theorem for you:</div>
+        </body>
+    </html>
+    """
 @app.get("/theorem")
 async def root():
-    return {"message": "Pythagorean Theorem: There exist three numbers a b and c, not all the same, such that a^2 + b^2 = c^2 "}
+    return {"theorem": "Pythagorean Theorem: There exist three numbers a b and c, not all the same, such that a^2 + b^2 = c^2 "}
