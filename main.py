e823bfd8272b3bb846e2f0a93bda48460e4ebcf1
Josh Grant <joshua.m.grant@gmail.com>
2025-11-05 10:41:44 -0500

    basic working api

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from theorems-as-a-service!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+@app.get('/index.html')
+async def root():
+    return {"message": "Welcome to Theorems as a Service"}
+
+@app.get("/theorem")
+async def root():
+    return {"message": "Pythagorean Theorem: There exist three numbers a b and c, not all the same, such that a^2 + b^2 = c^2 "}
