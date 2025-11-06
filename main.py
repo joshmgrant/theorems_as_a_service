28fe3791bab8d98c106f5a336efcf810adba6a3a
Josh Grant <joshua.m.grant@gmail.com>
2025-11-06 11:32:04 -0500

    added favicon

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 from theorem_generator import random_theorem
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -15,17 +17,18 @@ async def theorem_html():
     <html>
         <head>
             <title>Theorems as a Service</title>
+            <link id="favicon" rel="icon" type="image/x-icon" href="static/favicon.ico">
             <script defer src="https://cdn.jsdelivr.net/npm/mathjax@4/tex-mml-chtml.js"></script>
         </head>
         <body>
-            <h1>Welcome to Theorems as a Service!</h1>
-            <div>
+            <h1 style="font-family: Arial;">Welcome to Theorems as a Service!</h1>
+            <div style="font-family: Arial;">
                 <p>Here's a math theorem for you:</p>
             </div>
-            <div>
+            <div style="font-family: Arial;">
                 <p>{theorem}</p>
             </div>
-            <div>
+            <div style="font-family: Arial;">
                <button onClick="window.location.reload();">Refresh Page For Another Theorem</button>
             </div>
         </body>
