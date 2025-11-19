9197640d688fc6eb69184be561a2b96573667b37
Josh Grant <joshua.m.grant@gmail.com>
2025-11-19 14:29:31 -0500

    added another theorem

--- theorem_generator.py
+++ theorem_generator.py
@@ -13,7 +13,9 @@ def random_theorem() -> str:
         "Fermat's Last Theorem: No three positive integers $a$, $b$, and $c$ satisfy the equation $a^n + b^n = c^n$ for any integer value of $n \\ge 3$",
         "Noether's Theorem: Every continuous symmetry of the action of a physical system with conservative forces has a corresponding conservation law",
         "Poincaré–Bendixson Theorem: Given a differentiable real dynamical system defined on an open subset of the plane, every non-empty compact ω-limit set of an orbit, which contains only finitely many fixed points, is either a fixed point, a periodic orbit, or a connected set composed of a finite number of fixed points together with homoclinic and heteroclinic orbits connecting these.",
-        "Taylor's Theorem in Several Dimensions: A function $f: R^n \\to R$ is differentiable at $a \\in R^n$ if and only if there exists a linear functional $L: R^n \\to R$ and a function $h: R^n \\to R$ such that $f(x) = f(a) + L(x - a) + h(x)||x-a||, \\lim_{{x \\to a}}h(x) = 0$."
+        "Taylor's Theorem in Several Dimensions: A function $f: R^n \\to R$ is differentiable at $a \\in R^n$ if and only if there exists a linear functional $L: R^n \\to R$ and a function $h: R^n \\to R$ such that $f(x) = f(a) + L(x - a) + h(x)||x-a||, \\lim_{{x \\to a}}h(x) = 0$.",
+        "Sharkovskii's Theorem: If $f$ is a discrete dynamical system on the real line and has a periodic point of period 2 and has a point of period 3, then there exist points of all periods, including chaotic orbits.",
+        
         # "Picard–Lindelöf Theorem: Let $D \\subseteq \\mathbb {{R}} \\times \\mathbb {{R}} ^{{n}}$, and $(t_0,y_0) \\in \\operatorname {{ int }}$. Let $f:D \\to \\mathbb{{R}}^n$ be a function that is continuous in $t$ and Lipschitz continuous in $y$ with the Lipschitz constant independent of $t$. Then there exists some $\\varepsilon > 0$ such that the initial value problem $y'(t)=f(t,y(t)), y(t_0)=y_0}$ has a unique solution $y(t)$ on the interval $[t_0-\\varepsilon ,t_0+\\varepsilon] $.",
     ]
 
